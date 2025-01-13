4040891f39c8915d9da95e2aa23421744daf4bce
Nitesh Rathod <niteshrathod15205@gmail.com>
2025-01-14 01:24:41 +0530

    added loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./loader.css";
-import nIcon from "../assets/icon.png"
+import nIcon from "../assets/icon.png";
 
 const Loader = () => {
   return (
     <div class="card">
       <div class="loader">
         <div>
-            <img src={nIcon} alt="nlogo" className="nlogo"/>
+          <img src={nIcon} alt="nlogo" className="nlogo" />
         </div>
         <div class="bounce"></div>
       </div>
